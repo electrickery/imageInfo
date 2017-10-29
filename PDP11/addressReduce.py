47b66f63a092130e282425e2df6e893b59840de4
Fred Jan Kraan <fjkraan@xs4all.nl>
2017-10-29 16:27:34 +0100

    allowed comment on code line

--- PDP11/addressReduce.py
+++ PDP11/addressReduce.py
@@ -4,8 +4,10 @@
 # Lines starting with a "#" are ignored.
 # usage: python addressReduce.py testfile.txt > /dev/ttyUSB0
 
-# This is address Reduce 0.1, tested with Python 2.7.12 and a KDF-11-A/DBV-J-11 set in a Minc-11 at 9600 Baud
+# This is addressReduce 0.2, tested with Python 2.7.12 and a KDF-11-A/DBV-J-11 set in a Minc-11 at 9600 Baud
 # fjkraan@xs4all.nl, 2017-10-29
+# new for 0.2:
+# - allowed comment in code line, separated by a #.
 
 from __future__ import print_function
 import sys
@@ -17,10 +19,14 @@ file = open(fileName, "r")
 oldAddress = 0o0
 
 for line in file: 
-    if line.startswith('#') or not line.strip():
+    line = line.strip()
+    hashIndex = line.find('#')
+    if not (hashIndex == -1):
+        line = line[:hashIndex]
+    if not line:
         continue
     slashIndex = line.find('/')
-    
+#    print (">> " + line),
     address = int(line[:slashIndex], 8)
     data    = line[slashIndex+1:].strip()
 #    print str(int(address, 8)) + "==" + str(oldAddress)
